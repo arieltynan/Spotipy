ddf84be65af41492c1d9c2dcfaaf92511b1da6f9
arieltynan <arieltynan@gmail.com>
2022-10-04 23:00:30 -0400

    Formatting Git test

--- Spotify Data Test.py
+++ Spotify Data Test.py
@@ -24,10 +24,10 @@ popularity = []
 track_id = []
 
 for i in range(0,10,10):
-    album_results = sp.search(q='year:2021 genre:indie genre:alt genre:rock', type='album', limit=10,offset=i)
-    for i, t in enumerate(album_results['albums']['items']):
+    track_results = sp.search(q='year:2022 genre:indie genre:alt genre:rock', type='track', limit=10,offset=i)
+    for i, t in enumerate(track_results['tracks']['items']):
         artist_name.append(t['artists'][0]['name'])
-        album_name.append(t['name'])
+        track_name.append(t['name'])
         track_id.append(t['id'])
         popularity.append(t['popularity'])
 
